af3086cbb755e2b82409a5eeb99a68ef53eced97
Al0kMishra <rogalokmishra2110@gmail.com>
2024-11-25 15:29:16 +0530

    these are the questions that i have done in DAA lab

---
+++ DAA Lab Experiments/EXP2/Q1.c
@@ -0,0 +1,1 @@
+/*1) Write iterative and recursive code of linear search. Perform time (best, worst, and average) and space complexity analysis of the code. In case of recursive approach, first write recurrence relation then solve it to get the time complexity./*
